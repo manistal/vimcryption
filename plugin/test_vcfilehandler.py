786a48c540b2e1d1388ca9fbf7d5da6b4deeeba7
Miguel Nistal <nistam328@gmail.com>
2018-04-10 10:51:45 -0400

    Added unit testing for vcfilehandler, need to add some more stuff in there at some point

---
+++ plugin/test_vcfilehandler.py
@@ -0,0 +1,47 @@
+"""
+"""
+
+import sys
+import base64
+import unittest
+from io import BytesIO
+
+
+class VimMock():
+    def __init__(self):
+        self.buffer = list()
+
+    @property
+    def current(self):
+        return self
+
+    def command(self, command):
+        pass
+
+    def eval(self, command):
+        return ""
+
+
+class TestVCFileHandler(unittest.TestCase):
+    """
+    Unit tests for vimcryption.VCFileHandler
+    """
+    def setUp(self):
+        # Mock Vim Library which doesnt exist outside vim
+        sys.modules['vim'] = VimMock()
+
+        # Set up the input
+        import vimcryption
+        self.VCF = vimcryption.VCFileHandler()
+        self.plaintext_file = BytesIO(b'dmltY3J5cHRlZA==SU9QQVNT\nLOLPLAINTEXT\n')
+        self.base64_file = BytesIO(b'dmltY3J5cHRlZA==QkFTRTY0\nTE9MQkFTRTY0\n')
+
+    def test_process_header_plaintext(self):
+        self.VCF.ProcessHeader(self.plaintext_file)
+        self.assertEquals(self.VCF.cipher_type, "IOPASS")
+
+    def test_process_header_base64(self):
+        self.VCF.ProcessHeader(self.base64_file)
+        self.assertEquals(self.VCF.cipher_type, "BASE64")
+
+
