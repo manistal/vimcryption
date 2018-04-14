e06cbdac107f3f794fa12783ab6debe94310c893
Tom Manner <zephrincochrane@gmail.com>
2018-04-14 22:15:22 +0000

    Renamed testsetup.py to setup.py and initial pass at travis yaml file.

--- run-tests.sh
+++ run-tests.sh
@@ -2,19 +2,21 @@ __setup_venv() {
   echo "----Creating Python$1 virtual envrionment in venv$1----"
   rm -rf venv$1
   virtualenv -p python$1 venv$1
+  . venv$1/bin/activate
   echo "----Installing vimcryption into venv$1----"
-  venv$1/bin/pip install nose2
-  venv$1/bin/python testsetup.py install
+  pip install .
   echo "----done----"
 }
 
 __do_tests() {
   echo "--Running Python$1 Tests--"
-  PYTHONDONTWRITEBYTECODE=1 venv$1/bin/nose2 -s plugin/
+  . venv$1/bin/activate
+  nose2 -s plugin/
   echo "---------Complete---------"
   echo
 }
 
+export PYTHONDONTWRITEBYTECODE=1
 __setup_venv 2
 __setup_venv 3
 echo
