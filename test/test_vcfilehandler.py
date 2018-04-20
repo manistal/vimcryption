44193a9a86110fb0ce200254c9fc5db439f22e3b
Miguel Nistal <nistam328@gmail.com>
2018-04-20 17:20:22 -0400

    Removed deprecated unit tests:

--- test/test_vcfilehandler.py
+++ test/test_vcfilehandler.py
@@ -22,26 +22,17 @@ class VimMock():
         return "IOPASS"
 
 
-#class TestVCFileHandler(unittest.TestCase):
-#    """
-#    Unit tests for vimcryption.VCFileHandler
-#    """
-#    def setUp(self):
-#        # Mock Vim Library which doesnt exist outside vim
-#        sys.modules['vim'] = VimMock()
-#
-#        # Set up the input
-#        import plugin.vimcryption as vimcryption 
-#        self.VCF = vimcryption.VCFileHandler() 
-#        self.plaintext_file = BytesIO(b'dmltY3J5cHRlZA==SU9QQVNT\nLOLPLAINTEXT\n')
-#        self.base64_file = BytesIO(b'dmltY3J5cHRlZA==QkFTRTY0CmxvbApsb2wKbG9sCgo=')
-#
-#    def test_process_header_plaintext(self):
-#        self.VCF.ProcessHeader(self.plaintext_file)
-#        self.assertEquals(self.VCF.cipher_type, "IOPASS")
-#
-#    def test_process_header_base64(self):
-#        self.VCF.ProcessHeader(self.base64_file)
-#        self.assertEquals(self.VCF.cipher_type, "BASE64")
-#
+class TestVCFileHandler(unittest.TestCase):
+    """
+    Unit tests for vimcryption.VCFileHandler
+    """
+    def setUp(self):
+        # Mock Vim Library which doesnt exist outside vim
+        sys.modules['vim'] = VimMock()
+
+        # Set up the input
+        import plugin.vimcryption as vimcryption 
+        self.VCF = vimcryption.VCFileHandler() 
+        self.plaintext_file = BytesIO(b'dmltY3J5cHRlZA==SU9QQVNT\nLOLPLAINTEXT\n')
+        self.base64_file = BytesIO(b'dmltY3J5cHRlZA==QkFTRTY0CmxvbApsb2wKbG9sCgo=')
 
