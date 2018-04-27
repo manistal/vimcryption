ace58fb232885800ebd30820303ea8ba7fdacfd7
Tom Manner <zephrincochrane@gmail.com>
2018-04-27 20:27:36 +0000

    Branching based on origin/gh-pages explicitely now.

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -1,7 +1,7 @@
 # Always recreate the gh-pages branch!
 BRANCH=$(git symbolic-ref -q --short HEAD || git describe --tags --exact-match 2> /dev/null || git rev-parse --short HEAD)
 git fetch origin gh-pages
-git checkout gh-pages
+git checkout -b gh-pages origin/gh-pages
 git pull -X theirs --no-edit
 git merge -X theirs --no-edit master
 DOCGEN=1 ./run-tests.sh $TRAVIS_PYTHON_VERSION
