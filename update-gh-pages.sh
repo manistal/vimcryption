967846db73a7943ab9840125d50aaae10e2dfda5
Tom Manner <zephrincochrane@gmail.com>
2018-04-27 15:52:26 +0000

    More update-gh-pages changes.

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -1,8 +1,8 @@
 # Always recreate the gh-pages branch!
 BRANCH=$(git symbolic-ref -q --short HEAD || git describe --tags --exact-match 2> /dev/null || git rev-parse --short HEAD)
 git checkout gh-pages
-git pull -X theirs
-git merge -X theirs master
+git pull -X theirs --no-edit
+git merge -X theirs --no-edit master
 #git checkout master
 #git checkout -B gh-pages master
 DOCGEN=1 ./run-tests.sh $TRAVIS_PYTHON_VERSION
