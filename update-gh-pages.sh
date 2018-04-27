b426e4d6d767b6c5a159ebe8c53fdefb2452f821
Tom Manner <zephrincochrane@gmail.com>
2018-04-27 15:48:03 +0000

    More update-gh-pages changes.

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -1,10 +1,12 @@
 # Always recreate the gh-pages branch!
 BRANCH=$(git symbolic-ref -q --short HEAD || git describe --tags --exact-match 2> /dev/null || git rev-parse --short HEAD)
+git checkout gh-pages
+git pull
 git checkout master
 git checkout -B gh-pages master
 DOCGEN=1 ./run-tests.sh $TRAVIS_PYTHON_VERSION
 git status
 git add doc/
-git commit -m "Travis CI updated python$TRAVIS_PYTHON_VERSION coverage report."
+git commit -m "Travis CI updated python$TRAVIS_PYTHON_VERSION coverage report on $(date)"
 git push
 git checkout $BRANCH
