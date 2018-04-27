b804634ce3681e1ec9390b7cf397116994eff146
Tom Manner <zephrincochrane@gmail.com>
2018-04-27 15:40:00 +0000

    generates and pushes back coverage documentation to the  branch.

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -1,7 +1,10 @@
 # Always recreate the gh-pages branch!
+BRANCH=$(git symbolic-ref -q --short HEAD || git describe --tags --exact-match 2> /dev/null || git rev-parse --short HEAD)
 git checkout master
 git checkout -B gh-pages master
 DOCGEN=1 ./run-tests.sh $TRAVIS_PYTHON_VERSION
-echo git add doc/
-echo git commit -m "Travis CI updated python$TRAVIS_PYTHON_VERSION coverage report."
-echo git push
+git status
+git add doc/
+git commit -m "Travis CI updated python$TRAVIS_PYTHON_VERSION coverage report."
+git push
+git checkout $BRANCH
