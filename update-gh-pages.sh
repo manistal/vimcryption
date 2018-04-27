f3b66edd5843c70ff3515fadb5a899a9171afb17
Tom Manner <zephrincochrane@gmail.com>
2018-04-27 21:03:41 +0000

    Some debug output added to help with debugging travis.

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -1,9 +1,10 @@
+#!/usr/bin/env bash
 # Always recreate the gh-pages branch!
 BRANCH=$(git symbolic-ref -q --short HEAD || git describe --tags --exact-match 2> /dev/null || git rev-parse --short HEAD)
 git fetch origin
 git remote -v
 git branch
-git checkout gh-pages
+git checkout "gh-pages" --
 git pull -X theirs --no-edit
 git merge -X theirs --no-edit master
 DOCGEN=1 ./run-tests.sh $TRAVIS_PYTHON_VERSION
